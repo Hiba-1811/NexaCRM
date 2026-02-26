1b67dda60b969e64e780f00c3ff7ccd6c6414adf
Hiba-1811 <ghribihiba53@gmail.com>
2026-02-26 01:02:04 +0100

    Setup router + pinia + axios with TypeScript

---
+++ CRM_frontend_vue/src/pages/Login.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const msg: string = "Login Page"
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
--- CRM_frontend_vue/src/router/index.ts
+++ CRM_frontend_vue/src/router/index.ts
@@ -1,8 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
+import type { RouteRecordRaw } from "vue-router"
+import Login from "../pages/Login.vue"
+import Dashboard from "../pages/Dashboard.vue"
+
+const routes: Array<RouteRecordRaw> = [
+  { path: "/", redirect: "/login" },
+  { path: "/login", component: Login },
+  { path: "/dashboard", component: Dashboard },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
